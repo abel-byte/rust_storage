e64b18fca5a062f3589e05358d9a620a65bebf94
daydaybetter <961432926@qq.com>
2023-03-23 12:37:44 +0800

    jmeter

---
+++ docs/shell/reset.sh
@@ -0,0 +1,8 @@
+killall rust_storage
+rm -rf 00/database
+rm -rf 01/database
+rm -rf 02/database
+rm -rf 03/database
+rm -rf 04/database
+rm -rf 05/database
+rm -rf log/*
--- docs/shell/start.sh
+++ docs/shell/start.sh
@@ -1,7 +1,7 @@
-CONFIG=config/0.toml nohup ./rust_storage >> log/0.log &
-CONFIG=config/1.toml nohup ./rust_storage >> log/1.log &
-CONFIG=config/2.toml nohup ./rust_storage >> log/2.log &
-CONFIG=config/3.toml nohup ./rust_storage >> log/3.log &
-CONFIG=config/4.toml nohup ./rust_storage >> log/4.log &
-CONFIG=config/5.toml nohup ./rust_storage >> log/5.log &
+CONFIG=00/default.toml nohup ./rust_storage >> log/0.log &
+CONFIG=01/default.toml nohup ./rust_storage >> log/1.log &
+CONFIG=02/default.toml nohup ./rust_storage >> log/2.log &
+CONFIG=03/default.toml nohup ./rust_storage >> log/3.log &
+CONFIG=04/default.toml nohup ./rust_storage >> log/4.log &
+CONFIG=05/default.toml nohup ./rust_storage >> log/5.log &
 tail -f log/*.log
